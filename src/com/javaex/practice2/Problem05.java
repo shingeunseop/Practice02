622b4cde58de3666af3f6b196940082964f112d1
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-06 16:53:06 +0900

    5문제 완료

--- src/com/javaex/practice2/Problem05.java
+++ src/com/javaex/practice2/Problem05.java
@@ -8,10 +8,13 @@ public class Problem05 {
 		int i=0;
 		for(i=0;i<arry.length;i++) {
 			arry[i]=(int)(Math.random()*45)+1;
-			for(int j=5;j>=1;j--) {
+			for(int j=1;j<i;j++) {
 				if(arry[i]==arry[j])
+					while(true){
 					arry[i]=(int)(Math.random()*45)+1;
-				
+					if(arry[i]!=arry[j])
+						break;
+					}
 			}
 			System.out.print(arry[i]+" ");
 			
